d8bebefcc456d9394671c32178e4ce8063f07a58
Thomas Tanner <tanner@gmx.net>
2015-03-18 19:51:10 +0100

    change to LGPL

--- setup.py
+++ setup.py
@@ -74,7 +74,7 @@ setuptools.setup(
     classifiers=[
         "Development Status :: 5 - Production/Stable",
         "Intended Audience :: Developers",
-        "License :: OSI Approved :: GNU General Public License v3 or later (GPLv3+)",
+        "License :: OSI Approved :: GNU Lesser General Public License v3 or later (LGPLv3+)",
         "Programming Language :: Python",
         "Programming Language :: Python :: 2",
         "Programming Language :: Python :: 3",
--- tests/conftest.py
+++ tests/conftest.py
@@ -1,3 +1,21 @@
+# -*- coding: utf-8 -*-
+
+# This file is part of kryptomime, a Python module for email kryptography.
+# Copyright © 2013,2014 Thomas Tanner <tanner@gmx.net>
+#
+# This program is free software: you can redistribute it and/or modify it
+# under the terms of the GNU General Public License as published by the Free
+# Software Foundation, either version 3 of the License, or (at your option)
+# any later version.
+# 
+# This program is distributed in the hope that it will be useful, but WITHOUT
+# ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or
+# FITNESS FOR A PARTICULAR PURPOSE. See the included LICENSE file for details.
+# 
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+# For more details see the file COPYING.
+
 sender='Foo <foo@localhost>'
 passphrase='mysecret'
 receiver='Bar <bar@localhost>'
